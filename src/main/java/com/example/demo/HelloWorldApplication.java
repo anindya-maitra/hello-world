f89f1a1e78eabbdee5ac3a4d1177b4ad4b8ab8b1
anindya-maitra <anindyamaitra007@gmail.com>
2024-02-06 12:26:10 +0530

    rest

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -6,8 +6,10 @@ import java.net.UnknownHostException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
